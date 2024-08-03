62e0900f584e5c16f0b0a0a1f92ee2d8f7629646
Yash Agarwal <84071013+yash206@users.noreply.github.com>
2024-08-03 17:51:58 +0530

    Create 1460. Make Two Arrays Equal by Reversing Subarrays.py

---
+++ 1460. Make Two Arrays Equal by Reversing Subarrays.py
@@ -0,0 +1,28 @@
+''' 
+Question -
+You are given two integer arrays of equal length target and arr. In one step, you can select any non-empty subarray of arr and reverse it. You are allowed to make any number of steps.
+Return true if you can make arr equal to target or false otherwise.
+'''
+
+
+class Solution:
+    def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
+        target_freq = {}
+        for i in target:
+            if (i in target_freq):
+                target_freq[i]+=1
+            else:
+                target_freq[i]=1
+
+        arr_freq = {}
+        for i in arr:
+            if (i in arr_freq):
+                arr_freq[i]+=1
+            else:
+                arr_freq[i]=1
+        for key, value in target_freq.items():
+            if key not in arr_freq:
+                return False
+            if arr_freq[key] != value:
+                return False
+        return True
