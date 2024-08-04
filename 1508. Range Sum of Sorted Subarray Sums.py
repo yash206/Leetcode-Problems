f7c67c9f040c85248cf9b726d4b48b2a70a3de7d
Yash Agarwal <84071013+yash206@users.noreply.github.com>
2024-08-05 00:05:05 +0530

    Optimized

--- 1508. Range Sum of Sorted Subarray Sums.py
+++ 1508. Range Sum of Sorted Subarray Sums.py
@@ -7,16 +7,12 @@ Return the sum of the numbers from index left to index right (indexed from 1), i
 
 class Solution:
     def rangeSum(self, nums: List[int], n: int, left: int, right: int) -> int:
-        subarrays_sum = []
-        start = 0
-        for start in range(n):
-            for end in range(start, n):
-                temp = sum(nums[start:end+1])
-                subarrays_sum.append(temp)
-
-        subarrays_sum.sort()
-        ans = sum(subarrays_sum[left-1:right])
-        M = 10**9+7
-        ans = ans%M
-
-        return ans
+        mod = 10**9+7
+        ans = []
+        for i in range(n):
+            summ = 0
+            for j in range(i,n):
+                summ += nums[j]
+                ans.append(summ % mod)
+        ans.sort()
+        return sum(ans[left-1:right]) % mod
