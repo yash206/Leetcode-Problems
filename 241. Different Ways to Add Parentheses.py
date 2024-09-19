ac0c7db403fe88484b38481ef96c3086e278ccf9
Yash Agarwal <84071013+yash206@users.noreply.github.com>
2024-09-20 01:30:41 +0530

    Create 241. Different Ways to Add Parentheses.py

---
+++ 241. Different Ways to Add Parentheses.py
@@ -0,0 +1,39 @@
+''' 
+Question -
+Given a string expression of numbers and operators, return all possible results from computing all the different possible ways to group numbers and operators. You may return the answer in any order.
+The test cases are generated such that the output values fit in a 32-bit integer and the number of different results does not exceed 104.
+'''
+
+'''
+Difficulty - Medium
+'''
+
+
+class Solution:
+    def diffWaysToCompute(self, expression: str) -> List[int]:
+        res = []
+        # ans = []
+        for i in range(len(expression)):
+            oper = expression[i]
+            if oper == "+" or oper == "-" or oper == "*":
+                sub_str1 = expression[0 : i]
+                sub_str2 = expression[i + 1 : ]
+                s1 = self.diffWaysToCompute(sub_str1)
+                s2 = self.diffWaysToCompute(sub_str2)
+                for i in s1:
+                    for j in s2:
+                        if oper == "*":
+                            res.append(int(i) * int(j))
+                        if oper == "+":
+                            res.append(int(i) + int(j))
+                        if oper == "-":
+                            res.append(int(i) - int(j))
+        if len(res) == 0:
+            res.append(int(expression))
+        # print(res)
+        return res
+
+
+'''
+Time Complexity - O(2ⁿ)
+'''
