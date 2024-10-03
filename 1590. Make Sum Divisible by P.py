a976738a7271364160d9969368f2373533d21049
Yash Agarwal <84071013+yash206@users.noreply.github.com>
2024-10-04 01:49:19 +0530

    Create 1590. Make Sum Divisible by P.py

---
+++ 1590. Make Sum Divisible by P.py
@@ -0,0 +1,40 @@
+''' 
+Question -
+Given an array of positive integers nums, remove the smallest subarray (possibly empty) such that the sum of the remaining elements is divisible by p. It is not allowed to remove the whole array.
+Return the length of the smallest subarray that you need to remove, or -1 if it's impossible.
+A subarray is defined as a contiguous block of elements in the array.
+'''
+
+'''
+Difficulty - Medium
+'''
+
+
+class Solution:
+    def minSubarray(self, nums: List[int], p: int) -> int:
+        totalSum = sum(nums)
+        rem = totalSum % p
+
+        if rem == 0:
+            return 0
+
+        prefixMod = {0: -1}
+        prefixSum = 0
+        minLength = len(nums)
+
+        for i, num in enumerate(nums):
+            prefixSum += num
+            currentMod = prefixSum % p
+            targetMod = (currentMod - rem + p) % p
+
+            if targetMod in prefixMod:
+                minLength = min(minLength, i - prefixMod[targetMod])
+
+            prefixMod[currentMod] = i
+
+        return minLength if minLength < len(nums) else -1
+
+
+'''
+Time Complexity - O(n)
+'''
