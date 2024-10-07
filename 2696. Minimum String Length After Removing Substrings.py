b7947e28fcba81c6ddb80e6e783c55cec5d79f83
Yash Agarwal <84071013+yash206@users.noreply.github.com>
2024-10-08 01:04:52 +0530

    Create 2696. Minimum String Length After Removing Substrings.py

---
+++ 2696. Minimum String Length After Removing Substrings.py
@@ -0,0 +1,32 @@
+''' 
+Question -
+You are given a string s consisting only of uppercase English letters.
+You can apply some operations to this string where, in one operation, you can remove any occurrence of one of the substrings "AB" or "CD" from s.
+Return the minimum possible length of the resulting string that you can obtain.
+Note that the string concatenates after removing the substring and could produce new "AB" or "CD" substrings.
+'''
+
+'''
+Difficulty - Easy
+'''
+
+
+class Solution:
+    def minLength(self, s: str) -> int:
+        stack = []
+        for c in s:
+            if not stack:
+                stack.append(c)
+                continue
+            if c == "B" and stack[-1] == "A":
+                stack.pop()
+            elif c == "D" and stack[-1] == "C":
+                stack.pop()
+            else:
+                stack.append(c)
+        return len(stack)
+
+
+'''
+Time Complexity - O(n)
+'''
