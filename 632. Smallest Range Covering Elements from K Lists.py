f671d64323347699e8462b21abb52baca64ce09d
Yash Agarwal <84071013+yash206@users.noreply.github.com>
2024-10-14 01:27:37 +0530

    Create 632. Smallest Range Covering Elements from K Lists.py

---
+++ 632. Smallest Range Covering Elements from K Lists.py
@@ -0,0 +1,35 @@
+''' 
+Question -
+You have k lists of sorted integers in non-decreasing order. Find the smallest range that includes at least one number from each of the k lists.
+We define the range [a, b] is smaller than range [c, d] if b - a < d - c or a < c if b - a == d - c.
+'''
+
+'''
+Difficulty - Medium
+'''
+
+
+class Solution:
+    def smallestRange(self, nums: List[List[int]]) -> List[int]:
+        heap = []
+        cur_max = float('-inf')
+        for i in range(len(nums)):
+            heapq.heappush(heap, (nums[i][0], i, 0)) 
+            cur_max = max(cur_max, nums[i][0])
+        small = [float('-inf'), float('inf')]
+        while heap:
+            cur_min, list_idx, i = heapq.heappop(heap)
+            if cur_max - cur_min < small[1] - small[0]:
+                small = [cur_min, cur_max]
+            if i + 1 < len(nums[list_idx]):
+                nxt = nums[list_idx][i + 1]
+                heapq.heappush(heap, (nxt, list_idx, i+1))
+                cur_max = max(cur_max, nxt)
+            else:
+                break
+        return small
+
+
+'''
+Time Complexity - O(n*log(k))
+'''
