f11ea4725070243301b52417504118d0b157056d
Yash Agarwal <84071013+yash206@users.noreply.github.com>
2024-10-21 00:07:07 +0530

    Create 1106. Parsing A Boolean Expression.py

---
+++ 1106. Parsing A Boolean Expression.py
@@ -0,0 +1,47 @@
+''' 
+Question -
+A boolean expression is an expression that evaluates to either true or false. It can be in one of the following shapes:
+'t' that evaluates to true.
+'f' that evaluates to false.
+'!(subExpr)' that evaluates to the logical NOT of the inner expression subExpr.
+'&(subExpr1, subExpr2, ..., subExprn)' that evaluates to the logical AND of the inner expressions subExpr1, subExpr2, ..., subExprn where n >= 1.
+'|(subExpr1, subExpr2, ..., subExprn)' that evaluates to the logical OR of the inner expressions subExpr1, subExpr2, ..., subExprn where n >= 1.
+Given a string expression that represents a boolean expression, return the evaluation of that expression.
+It is guaranteed that the given expression is valid and follows the given rules.
+'''
+
+'''
+Difficulty - Hard
+'''
+
+
+class Solution:
+    def parseBoolExpr(self, expression: str) -> bool:
+        st = deque()
+        for c in expression:
+            if c == "," or c == "(":
+                continue
+            if c in ["t", "f", "!", "&", "|"]:
+                st.append(c)
+            elif c == ")":
+                has_true = False
+                has_false = False
+                while st[-1] not in ["!", "&", "|"]:
+                    top_value = st.pop()
+                    if top_value == "t":
+                        has_true = True
+                    elif top_value == "f":
+                        has_false = True
+                op = st.pop()
+                if op == "!":
+                    st.append("t" if not has_true else "f")
+                elif op == "&":
+                    st.append("f" if has_false else "t")
+                else:
+                    st.append("t" if has_true else "f")
+        return st[-1] == "t"
+
+
+'''
+Time Complexity - O(n)
+'''
